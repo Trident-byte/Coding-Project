48fef013350349d9f5d81aff92b626b9c01acd13
Br1anChau <bchau40@stuy.edu>
2023-04-02 12:46:47 -0400

    Getter exceptions added

--- java/edu/stuy/lab6/Driver.java
+++ java/edu/stuy/lab6/Driver.java
@@ -27,8 +27,6 @@ public class Driver{
             System.out.println(arrayString);
             System.out.println();
 
-//            test3.addFirst(null);
-//            test3.addLast(null);
 
             MyDeque<String> test4 = new MyDeque<String>(2);
             test4.addFirst("James");
@@ -36,13 +34,120 @@ public class Driver{
             System.out.println(test4);
             test4.addLast("bye");
             System.out.println(test4);
-            test4.addFirst("Mr."); 
+            test4.addFirst("Mr.");
             System.out.println(test4);
 
             System.out.println();
+
+            MyDeque<String> q = new MyDeque<>();
+            q.addFirst("Hello");
+            q.addLast("James");
+            q.addFirst("Well");
+            q.addLast("Wane");
+
+            System.out.println(q.getFirst());
+            System.out.println(q.getLast());
+            System.out.println(q.size());
+            System.out.println(q.toString());
+            System.out.println(q.removeFirst());
+            System.out.println(q.removeLast());
+
+            MyDeque<String> test5 = new MyDeque<>();
+            test5.addFirst("Hello");
+            test5.addLast("James");
+            test5.addFirst("Well");
+            test5.addLast("Wane");
+            test5.addLast("what");
+            test5.addLast("is");
+            test5.addLast("your");
+            test5.addLast("problem");
+            test5.addLast("AI");
+            test5.addLast("will");
+            test5.addLast("be");
+            test5.addLast("the");
+            test5.addLast("future");
+            test5.addLast("of");
+            test5.addLast("computing");
+            test5.addFirst("Hola");
+            System.out.println(test5.size());
+            System.out.println(test5.toString());
+
+            Object[] array2= test5.getData();
+            String arrayString2 = "";
+            for(int i = 0; i < array2.length; i++){
+                arrayString2 += array2[i] + ", ";
+            }
+            System.out.println(arrayString2);
+            System.out.println();
+
+
         } catch(Exception e){
             e.printStackTrace();
         }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.removeFirst();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.removeLast();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.addFirst(null);
+        }catch(Exception e){
+            System.out.println("Error: Cannot insert a null element");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.addLast(null);
+        }catch(Exception e){
+            System.out.println("Error: Cannot insert a null element");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.getData();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.getEnd();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.getFirst();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.getLast();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
+
+        try{
+            MyDeque<String> test3 = new MyDeque<String>(10);
+            test3.getStart();
+        }catch(Exception e){
+            System.out.println("Error: The dequeue is empty so the method cannot be executed.");
+        }
     }
+
 
 }
--- java/edu/stuy/lab6/MyDeque.java
+++ java/edu/stuy/lab6/MyDeque.java
@@ -3,46 +3,46 @@ package edu.stuy.lab6;
 import java.util.NoSuchElementException;
 public class MyDeque<E>{
 
-      // Instance variables
-      private E[] data;
-      private int size;
-      private int start, end;
+    // Instance variables
+    private E[] data;
+    private int size;
+    private int start, end;
 
-      private final int DEFAULT_CAPACITY = 10;
-      // Constructors
-      @SuppressWarnings("unchecked")
-      public MyDeque(){ 
+    private final int DEFAULT_CAPACITY = 10;
+    // Constructors
+    @SuppressWarnings("unchecked")
+    public MyDeque(){
         data = (E[])new Object[DEFAULT_CAPACITY];
         size = 0;
         start = 0;
         end = 0;
-      }
-      @SuppressWarnings("unchecked")
-      public MyDeque(int initialCapacity){ 
-         data = (E[])new Object[initialCapacity];
-         size = 0;
-         start = 0;
-         end = 0;
-      }
-      
-      // Add a new value on the indicated side
-      public void addFirst(E element) throws NullPointerException{
+    }
+    @SuppressWarnings("unchecked")
+    public MyDeque(int initialCapacity){
+        data = (E[])new Object[initialCapacity];
+        size = 0;
+        start = 0;
+        end = 0;
+    }
+
+    // Add a new value on the indicated side
+    public void addFirst(E element) throws NullPointerException{
         nullElement(element);
         if(size == data.length){
-          resize();
+            resize();
         }
         if(start == 0){
             start = data.length - 1;
         }else{
-          start--;
+            start--;
         }
         data[start] = element;
-        size++; 
-      }
-      public void addLast(E element) throws NullPointerException{
+        size++;
+    }
+    public void addLast(E element) throws NullPointerException{
         nullElement(element);
         if(size == data.length){
-          resize();
+            resize();
         }
         if (end != 0 || data[end] != null) {
             end++;
@@ -50,76 +50,80 @@ public class MyDeque<E>{
 
         data[end] = element;
         size++;
-      }
-      
-      // // Remove and return the element
-      public E removeFirst() throws NoSuchElementException{ 
+    }
+
+    // // Remove and return the element
+    public E removeFirst() throws NoSuchElementException{
         emptyArray();
         E element = data[start];
         data[start] = null;
         start++;
         size--;
         return element;
-      }
-      public E removeLast() throws NoSuchElementException{ 
+    }
+    public E removeLast() throws NoSuchElementException{
         emptyArray();
         E element = data[end];
         data[end] = null;
         end--;
         size--;
         return element;
-      }
-      
-      // Return the element. Do not deleter elements.
-      public E getFirst(){ 
+    }
+
+    // Return the element. Do not deleter elements.
+    public E getFirst() throws NoSuchElementException{
+        emptyArray();
         return data[start];
-      }
-      public E getLast(){ 
+    }
+    public E getLast() throws NoSuchElementException{
+        emptyArray();
         return data[end];
-      }
-      
-      // Return deque size
-      public int size(){ 
+    }
+
+    // Return deque size
+    public int size(){
         return size;
-      }
-      
-      // // Getters
-  
-      // // Return data
-      public E[] getData(){ 
+    }
+
+    // // Getters
+
+    // // Return data
+    public E[] getData() throws NoSuchElementException{
+        emptyArray();
         return data;
-      }
-      // // Return start
-      public int getStart(){ 
+    }
+    // // Return start
+    public int getStart() throws NoSuchElementException{
+        emptyArray();
         return start;
-      }
-      // // Return end
-      public int getEnd(){ 
+    }
+    // // Return end
+    public int getEnd() throws NoSuchElementException{
         return end;
-      }
-      
-      // Resize the array. Double its length (data.length * 2)
-      @SuppressWarnings("unchecked")
-      private void resize() { 
+    }
+
+    // Resize the array. Double its length (data.length * 2)
+    @SuppressWarnings("unchecked")
+    private void resize() {
         E[] replacementArray = (E[])new Object[data.length*2];
         int startIndex = start;
         for(int i = 0; i < size; i++){
-          replacementArray[i] = data[startIndex];
-          if(startIndex == data.length-1){
-              startIndex = 0;
-          }
-          else{
-            startIndex++;
-          }
+            replacementArray[i] = data[startIndex];
+            if(startIndex == data.length-1){
+                startIndex = 0;
+            }
+            else{
+                startIndex++;
+            }
         }
         data = replacementArray;
         start = 0;
-        end = size;
+        end = size - 1;
 
-      }
-      
-      // Return the deque as string. Ex.:  "[hello, my, dear, friends!]"
-      public String toString(){ 
+    }
+
+    // Return the deque as string. Ex.:  "[hello, my, dear, friends!]"
+    public String toString(){
         int startIndex = start;
         String representation = "[";
         for(int i = 0; i < size; i++){
@@ -128,21 +132,21 @@ public class MyDeque<E>{
                 startIndex = 0;
             }
             else{
-              startIndex++;
+                startIndex++;
             }
         }
         return representation + "]";
-      }
-
-      public void nullElement(E element) throws NullPointerException{
-        if(element == null){
-          throw new NullPointerException("Cannot insert a null element.");
-        }
-      }
-
-      public void emptyArray() throws NoSuchElementException{
-        if(size == 0){
-          throw new NoSuchElementException("The dequeue is empty so the method cannot be executed.");
-        }
-      }
     }
+
+    public void nullElement(E element) throws NullPointerException{
+        if(element == null){
+            throw new NullPointerException("Cannot insert a null element.");
+        }
+    }
+
+    public void emptyArray() throws NoSuchElementException{
+        if(size == 0){
+            throw new NoSuchElementException("The dequeue is empty so the method cannot be executed.");
+        }
+    }
+}
